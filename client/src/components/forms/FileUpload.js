93223288d6517648dfd2687ecc3ac7f9f69c87ac
adzaa <adidzankic@gmail.com>
2021-05-19 19:08:50 +0200

    kurs 19.05

--- client/src/components/forms/FileUpload.js
+++ client/src/components/forms/FileUpload.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Resizer from 'react-image-file-resizer';
 import axios from 'axios';
 import {useSelector} from 'react-redux';
+import {Avatar, Badge} from "antd";
 
 
 const FileUpload = ({values, setValues, setLoading}) => {
@@ -31,7 +32,7 @@ const FileUpload = ({values, setValues, setLoading}) => {
                         {
                             headers: {
                             authtoken: user ? user.token : "",
-                            },
+                            },                      
                         }
                     )
                         .then(res => {
@@ -51,22 +52,68 @@ const FileUpload = ({values, setValues, setLoading}) => {
         //send back to server to upload to clouidinary
         //set url to images[]
     };
+  
+    const handleImageRemove = (public_id) => {
+       
+        setLoading(true);
+        // console.log("remove imagee", id)
+            axios.post(
+               
+                `${process.env.REACT_APP_API}/removeimages`, 
+                { public_id }, 
+                {
+            headers: {
+                authtoken: user ? user.token : "",
+                },
+                }
+            )
+            
+        .then((res) => {
+        
+            setLoading(false)
+            const { images } = values;
+                let filteredImages = images.filter((item) => {
+                    return item.public_id !== public_id;
+                });
+                setValues({ ...values, images: filteredImages });
+         })
+        .catch((err) => {
+            console.log(err);
+            setLoading(false);
+        });
+            };
+        
 
     return (
+       <>
+        <div className="row">
+            {values.images && 
+                values.images.map((image) => (
+                <Badge count="X" key={image.public_id} 
+                onClick={() => 
+                handleImageRemove(image.public_id)}
+                style={{ cursor:"pointer" }}
+                >
+                <Avatar  src={image.url} size={100} 
+                className="ml-3"
+                shape="square"
+                /> </Badge> 
+            ))}
+        </div>
+      <hr/>
         <div className="row">
             <label className="btn btn-primary btn-raised"> 
                 Choose file 
-
                 <input 
                     type="file" 
                     multiple 
                     hidden 
                     accept="images/*" 
                     onChange={fileUploadAndResize} 
-                />
-            </label>
-            
+                /> 
+            </label>         
         </div>
+        </>
     )};
 
 export default FileUpload;
